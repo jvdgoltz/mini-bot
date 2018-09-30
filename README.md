ce05246cc9111a24c0dc32d3315950997690fd20
J.v.d.Goltz <j.vdgoltz@gmail.com>
2018-09-30 15:41:04 +0200

    add MiniBot in mini_bot.py as core class to control the robot

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # mini-bot
 Hobby project for a (Raspberry PI) controlled robot arm that tracks finger locations and movements from a webcam
+
+Used for this robot kit including its RPi servo controller:
+http://anleitung.joy-it.net/?goods=roboterarm-robot02
+
+Controller drivers:
+https://github.com/adafruit/Adafruit_Python_PCA9685
+
+
---
+++ mini_bot.py
@@ -0,0 +1,52 @@
+import time 
+
+import Adafruit_PCA9685
+
+def load_calib(id=0, calib_dir=None):
+	pass
+
+
+class MiniBot():
+	def __init__(self, n_servos=5, calib_dir=None):
+		#import calibration files for every servo in calib_dir
+		#initialize the pwm controller
+		pass
+
+	def init():
+		#set joints to default starting position
+		pass
+
+	def shutdown():
+		# put joints into safe state 
+		pass
+
+	def set_as_default():
+		pass
+
+
+	def set_joint_states():
+		pass
+
+	def get_joint_states():
+		pass
+
+	def set_ee_pose():
+		pass
+
+	def get_ee_pose():
+		pass
+
+	def close_claw():
+		pass
+
+	def open_claw():
+		pass
+
+	def set_claw():
+		pass
+
+def main():
+	return 0
+
+if __name__ == '__main__':
+	main()
